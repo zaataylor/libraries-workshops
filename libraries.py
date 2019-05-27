a4b6278cfdabc0468b7aa871d0847a5d3c3199c9
Zachary Taylor <zataylor@ncsu.edu>
2019-05-27 17:11:07 -0400

    Fix for getting workshop description

--- libraries.py
+++ libraries.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # When run, this script will give a printout of the workshops happening at the NCSU
-#libraries that appear on the first page
+# libraries that appear on the first page
 
 from bs4 import BeautifulSoup
 import urllib.request as url
@@ -9,23 +9,23 @@ import urllib.request as url
 rootURL = "https://www.lib.ncsu.edu"
 upcomingWkshpsURL = "https://www.lib.ncsu.edu/workshops/upcoming"
 with url.urlopen(upcomingWkshpsURL) as response:
-    #read in page contents
+    # read in page contents
     html = response.read()
     soup = BeautifulSoup(html, 'html.parser')
-    
-    #get title, date, and time of each workshop on the page
+
+    # get title, date, and time of each workshop on the page
     workshopTitles = soup.findAll(class_="evt-title")
     workshopDates = soup.findAll(class_="evt-date")
     workshopTimes = soup.findAll(class_="evt-time")
 
-    #for each workshop, print out title, date, and time
+    # for each workshop, print out title, date, and time
     for (title, date, time) in (zip(workshopTitles, workshopDates, workshopTimes)):
         print("Event Title: " + title.string)
         print("Event Date: " + date.string)
         print("Event Time: " + time.string)
         print(rootURL + title.a.get("href"))
-        
-        #get and print out the location, description, and registration link of each workshop
+
+        # get and print out the location, description, and registration link of each workshop
         with url.urlopen(rootURL + title.a.get("href")) as wkshp:
             wkshpHTML = wkshp.read()
             wkshpSoup = BeautifulSoup(wkshpHTML, 'html.parser')
@@ -35,9 +35,18 @@ with url.urlopen(upcomingWkshpsURL) as response:
                 'a[href*="https://reporter.ncsu.edu/"]')
             print("Location: {}".format(
                 workshopLocation[0].get_text().replace("\n", "")))
-            workshop_description = wkshpSoup.select(".field-item p")
+            # workshop_description = wkshpSoup.select('a[h3="Workshop Description"]')
+            # find page element(s) corresponding to the words "Workshop Description" and then
+            # find the next element of class field item and get the text from it
+            workshop_description = wkshpSoup.find(
+                "h3", text="Workshop Description")
+            workshop_description = workshop_description.next_sibling.next_sibling.find_all(
+                "p")
+            textBody = ""
+            for paragraph in workshop_description:
+                textBody += paragraph.get_text()
             print("Event Description: {}".format(
-                workshop_description[0].get_text()))
+                textBody))
             print("Registration Link: {}".format(
                   workshopRegistrationLink[0].get("href")))
             print("\n----------------------------------------\n")
